8abf9ba0cc8656944df5ff93440487a4ff796c07
sarabluekens <sara.bluekens@skynet.be>
2021-08-28 15:04:19 +0200

    css homepage added

--- src/view/makerfaire/index.php
+++ src/view/makerfaire/index.php
@@ -22,41 +22,44 @@
     <div class="workshop__info">
 
       <div class="overflow">
-        <!-- <img class="workshop__title--dark" src="../../assets/img/DIY.png" alt="tekst in de achtergrond" width="413" height="206"> -->
-        <h2 class="workshop__title--white"> Saxofoon</h2>
+        <h2 class="workshop__title--white subTitle"> Saxofoon</h2>
       </div>
 
       <p class="workshop__text">Voor de creatievelingen onder jullie die niet langer meer kunnen stil zitten, bieden we een ”Coronaproof” workshop aan. Raak met deze tutorial al helemaal in de creatieve sfeer en maak je eigen Saxofoon! </p>
-      <a class="workshop__button button" href="index.php?page=tutorial"><img  src="../assets/img/naarTutorial.png" alt="Stuurt je naar de tutorial pagina " width="344" height="84"></a>
+      <a class="workshop__button button" href="index.php?page=tutorial"><img  src="../assets/img/naarTutorial.svg" alt="Stuurt je naar de tutorial pagina " width="344" height="84"></a>
     </div>
 
     <img class="workshop__image" src="../../assets/img/RobotSax.png" alt="Teaser van de saxofoon met het Maker Faire robotje" width="725" height="556">
   </section>
 
   <article class="promo">
-    <h2 class="promo__title">Festival der creatievelingen</h2>
+  <h2 class="visually-hidden">Maker Faire promo</h2>
+
     <div class="promo__flex row__flex">
-      <img class="promo__image" src="../assets/img/remote.png" alt="afstandsbediening die een andere image op de site bestuurd." width="504" height="527">
-      <div class="column__flex">
-        <p class="promo__text">De Maker Faire Gent is een technologisch doe-het-zelf festival, waar mensen samenkomen om hun creaties tentoon te stellen. Ook kan je workshops volgen, zelf demootjes knutselen of luisteren naar keynote speeches.</p>
-        <p class="promo__text">Je kan er alles terugvinden dat met creatie te maken heeft: van een elektrisch longboard tot een muurschildering tot een zelfgemaakte Nintendo. </p>
+      <img class="promo__image" src="../assets/img/remote.png" alt="afstandsbediening die een andere image op de site bestuurd." width="446" height="557">
+      <div class="flex__column">
+      <h2 class="promo__title promo__style subTitle">Festival der creatievelingen</h2>
+
+        <p class="promo__text promo__style">De Maker Faire Gent is een technologisch doe-het-zelf festival, waar mensen samenkomen om hun creaties tentoon te stellen. Ook kan je workshops volgen, zelf demootjes knutselen of luisteren naar keynote speeches.</p>
+        <p class="promo__text promo__style">Je kan er alles terugvinden dat met creatie te maken heeft: van een elektrisch longboard tot een muurschildering tot een zelfgemaakte Nintendo. </p>
+        <a class="promo__button button" href="https://www.makerfairegent.be"><img src="../../assets/img/naarMakerFaire.svg"  alt="Naar de maker faire pagina "width="260" height="90"></a>
       </div>
+
     </div>
-    <a class="promo__button button" href="https://www.makerfairegent.be"><img src="../../assets/img/leerMeer.svg"  alt="Naar de maker faire pagina "width="349" height="121"></a>
     </article>
 
-  <img class="car" src="../assets/img/car.png" alt="autootje" width="">
+  <img class="car" src="../assets/img/autootje.png" alt="autootje" width="1400" height="233">
   <section class="sponsors">
     <h2 class=" visually-hidden"> Sponsors</h2>
     <div class="sponsors__img">
       <!-- nog een lottie van het bewegend karretje als er geklicked wordt op de afstandsbediening -->
-      <a href="https://www.accentjobs.be/nl"><img class="footer__socialMedia--item" src="../assets/img/accent.png" alt="accent" width="210" height="89"></a>
-      <a href="https://www.velleman.eu/"><img class="footer__socialMedia--item" src="../assets/img/velleman.png" alt="velleman" width="210" height="88"></a>
-      <a href="https://makezine.com/"><img class="footer__socialMedia--item" src="../assets/img/makeazine.png" alt="makeazine" width="180" height="88"></a>
-      <a href="https://www.detentzetter.be/"><img class="footer__socialMedia--item" src="../assets/img/tentzetter.png" alt="tentzetter" width="200" height="88"></a>
-      <a href="https://stad.gent/nl"><img class="footer__socialMedia--item" src="../assets/img/gent.png" alt="gent" width="168" height="88"></a>
-      <!-- <a href="https://www.vlaanderen.be/en"><img class="footer__socialMedia--item" src="../assets/img/flanders.png" alt="flanders" width="210" height="89"></a> -->
-      <!-- <a href="https://www.ugent.be/"><img class="footer__socialMedia--item" src="../assets/img/uniGent.png" alt="Universiteit gent" width="181" height="89"></a> -->
+      <a href="https://www.accentjobs.be/nl"><img class="sponsors__img--item" src="../assets/img/accent.png" alt="accent" width="210" height="89"></a>
+      <a href="https://www.velleman.eu/"><img class="sponsors__img--item" src="../assets/img/velleman.png" alt="velleman" width="210" height="88"></a>
+      <a href="https://makezine.com/"><img class="sponsors__img--item" src="../assets/img/makeazine.png" alt="makeazine" width="180" height="88"></a>
+      <a href="https://www.detentzetter.be/"><img class="sponsors__img--item" src="../assets/img/tentzetter.png" alt="tentzetter" width="200" height="88"></a>
+      <a href="https://stad.gent/nl"><img class="sponsors__img--item" src="../assets/img/gent.png" alt="gent" width="168" height="88"></a>
+      <a href="https://www.vlaanderen.be/en"><img class="sponsors__img--item" src="../assets/img/flanders.png" alt="flanders" width="210" height="89"></a>
+      <a href="https://www.ugent.be/"><img class="sponsors__img--item" src="../assets/img/uniGent.png" alt="Universiteit gent" width="181" height="89"></a>
     </div>
   </section>
 
