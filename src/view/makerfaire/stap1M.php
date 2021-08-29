ba11fb2a8a3206a30c7e604d173d03a79bb93fc6
sarabluekens <sara.bluekens@skynet.be>
2021-08-29 12:18:46 +0200

    tutorial added M

--- src/view/makerfaire/stap1M.php
+++ src/view/makerfaire/stap1M.php
@@ -1,8 +1,26 @@
 <article class="stap1">
 <h2 class="visually-hidden">Stap1</h2>
+<a class="overzicht__button--terug" href="index.php?page=overzichtM"><img src="../assets/img/terug.svg" alt="terug knop die teugkeert naar de homepage" width="107" height="37"></a>
 
-<section class="overzicht">
-  <h2 class="overzicht__title subTitle">Stap1</h2>
-  <p class="overzicht__title ">Kies zelf de volgorde waarin je deze tutorial wilt volgen!</p>
-  <a class="overzicht__button--terug" href="index.php?page=overzichtM"><img src="../assets/img/terug.svg" alt="terug knop die teugkeert naar de homepage" width="107" height="37"></a>
+<section class="stap">
+  <h2 class="stap__title subTitle">Mondstuk</h2>
+
+  <div class="stap__flex">
+    <section class="stap__deel">
+      <h2 class="visually-hidden">Deel 1</h2>
+      <img class="stap__deel--img buis" src="../assets/img/stap1.1.png" alt="foto van het zagen van een pvc buis." width="308" height="517">
+      <p class="stap__deel--text">Zaag een top van de PVC buis met een hoek van ongeveer 30°.</p>
+    </section>
+    <section class="stap__deel">
+      <h2 class="visually-hidden">Deel 2</h2>
+      <img class="stap__deel--img" src="../assets/img/stap1.2.png" alt="foto van het knippen van de vorm van de buis uit plastiek" width="949" height="1080">
+      <p class="stap__deel--text">Knip de vorm van het gezaagde stuk uit het verhard plastiek.</p>
+
+    </section>
+    <section class="stap__deel">
+      <h2 class="visually-hidden">Deel 3</h2>
+      <img class="stap__deel--img klein" src="../assets/img/stap1.3.png" alt="foto van het zagen van een pvc buis." width="949" height="646">
+      <p class="stap__deel--text">Kleef het plastiek op de buis maar laat een klein gaatje over ! <span class="stap__deel--span"> (Hierdoor zal je blazen)</span></p>
+    </section>
+  </div>
 </section>
--- src/view/makerfaire/stap2M.php
+++ src/view/makerfaire/stap2M.php
@@ -1,8 +1,26 @@
 <article class="stap1">
-<h2 class="visually-hidden">Stap1</h2>
+<h2 class="visually-hidden">Stap2</h2>
+<a class="overzicht__button--terug" href="index.php?page=overzichtM"><img src="../assets/img/terug.svg" alt="terug knop die teugkeert naar de homepage" width="107" height="37"></a>
 
-<section class="overzicht">
-  <h2 class="overzicht__title subTitle">Stap1</h2>
-  <p class="overzicht__title ">Kies zelf de volgorde waarin je deze tutorial wilt volgen!</p>
-  <a class="overzicht__button--terug" href="index.php?page=overzichtM"><img src="../assets/img/terug.svg" alt="terug knop die teugkeert naar de homepage" width="107" height="37"></a>
+<section class="stap">
+  <h2 class="stap__title subTitle">Body</h2>
+
+  <div class="stap__flex body">
+    <section class="stap__deel ">
+      <h2 class="visually-hidden">Deel 1</h2>
+      <img class="stap__deel--img klein" src="../assets/img/stap2.1.png" alt="foto van het zagen van een pvc buis." width="308" height="517">
+      <p class="stap__deel--text">Lijm het mondstuk op een hoekstuk.</p>
+    </section>
+    <section class="stap__deel ">
+      <h2 class="visually-hidden">Deel 2</h2>
+      <img class="stap__deel--img" src="../assets/img/stap2.2.png" alt="foto van het knippen van de vorm van de buis uit plastiek" width="949" height="1080">
+      <p class="stap__deel--text">Lijm de pvc buizen op de hoek- stukken (met het mondstuk vanboven)</p>
+
+    </section>
+    <section class="stap__deel">
+      <h2 class="visually-hidden">Deel 3</h2>
+      <img class="stap__deel--img klein" src="../assets/img/stap2.3.png" alt="foto van het zagen van een pvc buis." width="949" height="646">
+      <p class="stap__deel--text">Lijm de hoorn op het andere hoekstuk bovenaan de saxofoon. <span class="stap__deel--span"> (Hierdoor zal je blazen)</span></p>
+    </section>
+  </div>
 </section>
